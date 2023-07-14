3416749701b9ab4fb383d493af5f071b1f079711
student <student@gnu.ac.in>
2023-07-14 12:33:03 +0530

    hey

---
+++ src/main/kotlin/Practical-1.1.kt
@@ -0,0 +1,21 @@
+fun main(){
+    var a = 22;
+    var f = 1.5;
+    var c = 'T';
+    var s = "HMS";
+    var g = false;
+    var d = 96.36;
+    var l = 338927847;
+    var h = -2;
+    var t = 127;
+    println("Integer Value: $a")
+    println("Float Value: $f")
+    println("Character Value: $c")
+    println("String Value: $s")
+    println("Boolean Value: $g")
+    println("Double Value: $d")
+    println("Long Value : $l")
+    println("Short Value: $h")
+    println("Byte Value: $t")
+
+}
