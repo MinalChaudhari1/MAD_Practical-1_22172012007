0fa34686076ce20aeed5a22c4b42cc6a3473e526
student <student@gnu.ac.in>
2023-07-19 09:00:41 +0530

    hey

---
+++ src/main/kotlin/practical1.3.kt
@@ -0,0 +1,8 @@
+fun main(){
+    println("Enter number: ")
+    var sEnrollmentNumber=readLine()!!.toInt()
+    println(" Student Enrollment No. : $sEnrollmentNumber")
+    println("Enter Name:")
+    val name = readLine()
+    println("Student name: $name")
+}
